63cc7775f52d65d15aa4bc7d24f427580e09d294
SoultaneRaqi <soultaneraqi1@gmail.com>
2026-03-12 02:35:05 +0000

    Update bin/index.js (The Entry Point)

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,7 @@
 
 import { Command } from 'commander';
 import chalk from 'chalk';
-import inquirer from 'inquirer'; // We added this import
+import { initCommand } from '../src/commands/init.js'; // Importing our logic
 
 const program = new Command();
 
@@ -11,48 +11,11 @@ program
   .description(chalk.cyan('🛠️  ForgeX: The Ultimate Node.js & Express Scaffolder'))
   .version('1.0.0');
 
+// Registering the init command
 program
   .command('init')
   .description('Initialize a new Express project structure')
-  .action(async () => { // Notice we made this function async
-    console.log(chalk.bgCyan.black(' ForgeX Init \n'));
-    
-    // The interactive prompts
-    const answers = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'projectName',
-        message: 'What is the name of your project?',
-        default: 'my-express-app',
-      },
-      {
-        type: 'list',
-        name: 'architecture',
-        message: 'Which architecture do you want to use?',
-        choices: [
-          'Standard (Layered: Controller-Service-Repository)',
-          'Advanced (Modular: Feature-based domains)'
-        ],
-      },
-      {
-        type: 'list',
-        name: 'packageManager',
-        message: 'Which package manager do you prefer?',
-        choices: ['npm', 'yarn', 'pnpm'],
-      }
-    ]);
-
-    // Output the choices back to the terminal
-    console.log('\n' + chalk.green('✔ Configuration saved:'));
-    console.log(chalk.cyan(`Project: ${answers.projectName}`));
-    
-    // We split the string to just get the word "Standard" or "Advanced"
-    const archType = answers.architecture.split(' ')[0]; 
-    console.log(chalk.cyan(`Architecture: ${archType}`));
-    console.log(chalk.cyan(`Manager: ${answers.packageManager}\n`));
-    
-    console.log(chalk.yellow(`Next step: Generating the ${archType} folder structure...`));
-  });
+  .action(initCommand);
 
 program.parse(process.argv);
 
